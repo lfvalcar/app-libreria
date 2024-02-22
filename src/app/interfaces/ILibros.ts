759ca1cb0e808fd04a853bb312f5b6bcbea283b3
lfvalcar <luisfervalcar@outlook.es>
2024-02-22 19:22:51 +0000

    interfaces actualizadas

---
+++ src/app/interfaces/ILibros.ts
@@ -0,0 +1,12 @@
+export interface ILibros {
+    isbn:             string;
+    title:            string;
+    pageCount:        number;
+    publishedDate:    string;
+    thumbnailUrl:     string;
+    shortDescription: string;
+    longDescription:  string;
+    status:           string;
+    precio:           number;
+    autor:            string;
+}
