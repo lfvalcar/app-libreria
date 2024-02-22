8343d9a51b2758a3157ca2832a40622defa14d88
lfvalcar <luisfervalcar@outlook.es>
2024-02-22 19:24:01 +0000

    componente header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import Menu from '@/app/components/Menu'
 
 const Header = () => {
   return (
     <header>
       <h2>Header Admin</h2>
-      { <Menu /> }
     </header>
   )
 }
